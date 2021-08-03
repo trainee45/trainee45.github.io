d5bf2337fa52607d355c8a46a8d21de37526d7bf
Doug Dyer <addyer@telus.net>
2021-08-02 21:42:11 -0700

    changed fetch code to original
    The added if statement caused errors

--- TableNotesVer29sw.js
+++ TableNotesVer29sw.js
@@ -44,37 +44,37 @@ self.addEventListener('install', function(e) {
 
 //service worker fetch code I know works! Below
 
-// self.addEventListener('fetch', (e) => {
-//   e.respondWith(
-//     caches.match(e.request).then((r) => {
-//           console.log('[Service Worker] Fetching resource: '+e.request.url);
-//       return r || fetch(e.request).then((response) => {
-//                 return caches.open(cacheName).then((cache) => {
-//           console.log('[Service Worker] Caching new resource: '+e.request.url);
-//           cache.put(e.request, response.clone());
-//           return response;
-//         });
-//       });
-//     })
-//   );
-// });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
 
 ////service worker fetch code I know works! Above
 
 
-//try this fetch code below
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
+//try this fetch code below NOPE INTRODUCED ERROR
+// self.addEventListener('fetch', (e) => {
+//   e.respondWith((async () => {
+//     const r = await caches.match(e.request);
+//     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//     if (r) { return r; }
+//     const response = await fetch(e.request);
+//     const cache = await caches.open(cacheName);
+//     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//     cache.put(e.request, response.clone());
+//     return response;
+//   })());
+// });
 
 
 //try this fetch code above
