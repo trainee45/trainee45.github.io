5fd781929c7e333c4666963261da674ecd46cd8e
Eeyore <trainee45@users.noreply.github.com>
2021-11-16 17:26:57 -0800

    tuning service worker
    Tuning service worker

--- DougieBaseVer36sw.js
+++ DougieBaseVer36sw.js
@@ -1,13 +1,30 @@
-//DougieBaseVer36sw.js added eyeore bkgrd image file Oct12 fudd4 Nov 5
-//from … TableNotesVer30sw.js
-var cacheName = 'DougieBase-v1'
-self.addEventListener('install', function(e) {
- e.waitUntil(
-//was 'video-store're'In the install listener, we can initialize the cache and add files to it for offline use. The service worker does not install until the code inside waitUntil is executed. It returns a promise — this approach is needed because installing may take some time, so we have to wait for it to finish. we open a cache with a given name, then add all the files our app uses to the cache, so they are available next time it loads (identified by request URL).
-//line below was —-caches.open('notes-os').then(function(cache) {..replaced notes-os with DougieBase-v1 via the variable cacheName Date:July 4 2021 the variable name does not have quotes ?correct????
-  caches.open(cacheName).then(function(cache) {
-     return cache.addAll([
-       '/',//https://github.com/trainee45/trainee45.github.io',//directory path to project folder
+/*from Copy of tryThisSw.js Nov16
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+ // 'index.html',
+  //'./', // Alias for index.html
+ // 'styles.css',
+ // '../../styles/main.css',
+//  'demo.js'
+ '/',//https://github.com/trainee45/trainee45.github.io',//directory path to project folder
        '/index.html',//directory path to project folder/app name.html file
 	   
 	    // 'https://github.com/trainee45/trainee45.github.io/TableNotesVer29.html',//directory path to project folder/app name.html file
@@ -17,111 +34,53 @@ self.addEventListener('install', function(e) {
 	   '/jquery-3.6.0.min.js',////directory path to project folder/app name.jQuery file
 	    '/eeyore.JPG',//add this file to github repository
 		'/fudd4.jpeg'
-     ]);
-   })
- );
+
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
 });
 
-//fetch event at our disposal, which fires every time an HTTP request is fired off from our app. This is very useful, as it allows us to intercept requests and respond to them with custom responses.The response can be anything we want: the requested file, its cached copy, or a piece of JavaScript code that will do something specific we serve content from the cache instead of the network as long as the resource is actually in the cache. We do this whether the app is online or offline. If the file is not in the cache, the app adds it there first before then serving it:
-
-/*self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
   );
-});  */
+});
 
-//Here, we respond to the fetch event with a function that tries to find the resource in the cache and return the response if it's there. If not, we use another fetch request to fetch it from the network, then store the response in the cache so it will be available there next time it is requested.
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-//The FetchEvent.respondWith method takes over control — this is the part that functions as a proxy server between the app and the network. This allows us to respond to every single request with any response we want: prepared by the Service Worker, taken from cache, modified if needed.
-
-
-//here is the complete code!
-
-
-
-
-
-
-//service worker fetch code I know works! Below
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
         });
-      });
-    })
-  );
+      })
+    );
+  }
 });
-
-////service worker fetch code I know works! Above
-
-
-//try this fetch code below NOPE INTRODUCED ERROR
-// self.addEventListener('fetch', (e) => {
-//   e.respondWith((async () => {
-//     const r = await caches.match(e.request);
-//     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-//     if (r) { return r; }
-//     const response = await fetch(e.request);
-//     const cache = await caches.open(cacheName);
-//     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-//     cache.put(e.request, response.clone());
-//     return response;
-//   })());
-// });
-
-
-//try this fetch code above
-
-
-
-/* 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
-
-
-*/
-
-//Updates
-
-//There is still one point to cover: how do you upgrade a Service Worker when a new version of the app containing new assets is available? The version number in the cache name is key to this:
-
-/*var cacheName = 'js13kPWA-v1';
-When this updates to v2, we can then add all of our files (including our new files) to a new cache:
-
-contentToCache.push('/pwa-examples/js13kpwa/icons/icon-32.png');
-
-// ...
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('js13kPWA-v2').then((cache) => {
-      return cache.addAll(contentToCache);
-    })
-  );
-}); */
-
-//A new service worker is installed in the background, and the previous one (v1) works correctly up until there are no pages using it — the new Service Worker is then activated and takes over management of the page from the old one.
-
-
